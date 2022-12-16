6fc7f991f6ce7f83344d8f851191d802db3e8abe
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-16 19:57:39 +0800

    Add files via upload

---
+++ Tags.lua
@@ -0,0 +1,22 @@
+return {
+  [""] = {
+    TagText = "VAPE OWNER",
+    TagColor = Color3.fromRGB(70,70,255)
+  },
+  [""] = {
+    TagText = "BLACK MONKEY",
+    TagColor = Color3.fromRGB(0,255,200)
+  },
+  [""] = {
+    TagText = "VAPE PRIVATE",
+    TagColor = Color3.fromRGB(255,255,0)
+  },
+  [""] = {
+    TagText = "AC MOD",
+    TagColor = Color3.fromRGB(0,115,255)
+  },
+  [""] = {
+    TagText = "AC MOD",
+    TagColor = Color3.fromRGB(255,0,255)
+  }
+}
